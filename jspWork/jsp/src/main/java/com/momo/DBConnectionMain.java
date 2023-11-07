e3e77c0e6fe35215a42cd29a01257b6e3d7919c2
GohWonil <kwi5124@naver.com>
2023-11-07 21:29:11 +0900

    jsp&orcl 연결 & 서블릿 포함

--- jspWork/jsp/src/main/java/com/momo/DBConnectionMain.java
+++ jspWork/jsp/src/main/java/com/momo/DBConnectionMain.java
@@ -8,43 +8,45 @@ import java.sql.Statement;
 
 public class DBConnectionMain {
 
+	
 	public static void main(String[] args) {
 		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
-		String id = "ORCLSTUDY";
-		String pw = "orcl";
-		
-		/*
+		String id = "test";
+		String pw = "1234";
+			
+		/* 
 		 * 1. 드라이버 로딩
-		 * 	DB에 접근하기 위해 필요한 라이브러리가 있는지 확인
+		 * 		DB에 접근하기 위해 필요한 라이브러리가 있는지 확인
+		 * 
 		 * 2. 커넥션 객체를 생성
-		 * 	
-		 * */
+		 * 
+		 */
 		try {
-			//1. 드라이버 로딩
+			// 1. 드라이버 로딩
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			
-			//2. 커넥션 생성
+			// 2. 커넥션 생성
 			Connection con = DriverManager.getConnection(url, id, pw);
-			
-			//3. 쿼리문장 준비
-			String sql = "SELECT sysdate from dual";
-			
-			//4. 쿼리 문장 실행
+
+			// 3. 쿼리문장 준비
+			String sql = "select sysdate from dual";
+
+			// 4. 쿼리 문장 실행
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(sql);
 			rs.next();
-			//숫자를 입력할 경우 1부터
-			//숫자 또는 결과 집합의 컬럼명을 getString(에 넣는다.)
+			// 숫자를 입력시 1부터
+			// 숫자또는 결과집합의 컬럼명
 			System.out.println(rs.getString("sysdate"));
-			//stmt.executeQuery(sql);
-			//System.out.println("con");
+			
+			
 		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패 - 라이브러리 어디있음");
+			System.out.println("드라이버 로딩실패 - 라이브러리를 찾을수 없습니다.");
 			e.printStackTrace();
 		} catch (SQLException e) {
-			System.out.println("연결 실패임");
+			System.out.println("Connection 객체 생성 실패");
 			e.printStackTrace();
 		}
+		
 	}
 
 }
